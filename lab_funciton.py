9c1908acfbe3169a69336b89eb540d50f0c00c05
Lama Alsharif <lama.a.alsharif@gmail.com>
2023-08-03 10:13:47 +0300

    the file has modified

--- lab_funciton.py
+++ lab_funciton.py
@@ -1,6 +1,7 @@
 
 def Print_number(number:int):
     '''This function it will print the numbers in revers and in inverted payramid  '''
+    print(Print_number.__doc__)
     for i in reversed(range(1 ,number+1)):
            for j in reversed(range(1 , i+1)):
               print(j , end=" ")
